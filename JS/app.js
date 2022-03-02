01e1930345506d751cf53383786f5969e0f3be5f
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-02 10:08:46 +0600

    Display weather

--- JS/app.js
+++ JS/app.js
@@ -5,11 +5,24 @@ const searchInput = document.getElementById('weather-input')
 /* Get Variables */
 const getVar = id => document.getElementById(id)
 
-// Set InnerText
+/*  Set InnerText */
 const innerText = (id, text) => getVar(id).innerText = text
 
 
 /* Search Weather Function */
 const searchWeather = () => {
-    innerText('city', 'Something')
+     
+    const API_KEY = `270df15f043e6cbd04e00a2a72c161a5`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${API_KEY}`;
+    fetch(URL)
+    .then(res => res.json())
+    .then(json => printWeather(json))
+}
+
+/* Print Data in UI */
+const printWeather = json => {
+    innerText('city', json.name)
+    innerText('temp', json.main.temp)
+    innerText('weather', json.weather[0].main)
+    getVar('icon').setAttribute('src', `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`)
 }
